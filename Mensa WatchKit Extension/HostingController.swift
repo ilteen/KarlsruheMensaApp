4c269163848934159f528b59cfbb7d456b5456d7
Philipp <uukfn@student.kit.edu>
2019-11-15 17:08:48 +0100

    Initial Commit

---
+++ Mensa WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Mensa WatchKit Extension
+//
+//  Created by Philipp on 15.11.19.
+//  Copyright © 2019 Philipp. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
