5255de11c8c0bcf6ff7d850c7c212da10875fd4f
Philipp <uukfn@student.kit.edu>
2020-11-11 08:53:18 +0100

    Updated Icons

--- Mensa/View/WeekDays.swift
+++ Mensa/View/WeekDays.swift
@@ -44,7 +44,6 @@ struct WeekDays: View {
                 }
             }
             .frame(maxWidth: .infinity)
-            .animation(Animation.default)
             //display the selected day in words
             Text(getSelectedDate(offset: self.selection, onlyDay: false)).font(.system(size: 17))
         }
